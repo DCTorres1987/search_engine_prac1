297d621a0439aee1c024c1404fadb5abdda4a7a9
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-08-05 15:47:23 -0400

    seperate sytled components to seperate file

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import './App.css';
 import JSONDATA from './data/MOCK_DATA.json';
 import {useState} from 'react';
-import styled from 'styled-components'
 import React from 'react';
+import {
+  INPUT_TEXT,
+  BEANIE_DIV,
+  BANNER_DIV
+} from '/styledcomponents/styl_component.js';
 
-const INPUT_TEXT = styled.input`
-  background: white;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  box-sizing: border-box;
-  display: inline-block;
-  margin-bottom: 50px;
-  margin-top: 50px;
-  padding: 12px 20px;
-  width: 50%;
-`;
-
-const BEANIE_DIV = styled.div `
-  background: white;
-  border: solid lightgray;
-  display: flex;
-  float: left;
-  height: 350px;
-  padding: 65px;
-  position: relative;
-  margin-left: 10%;
-  margin-top: 10px;
-  width: 350px;
-`;
-
-const BANNER_DIV = styled.div `
-  background-color: #89d4cf;
-  background-image: linear-gradient(315deg, #89d4cf 0%, #6e45e1 74%);
-  height: 150px;
-`;
 
 function App() {
  const [searchTerm, setSearchTerm] = useState('')
---
+++ src/styledcomponents/styl_component.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+
+export const INPUT_TEXT = styled.input`
+  background: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: inline-block;
+  margin-bottom: 50px;
+  margin-top: 50px;
+  padding: 12px 20px;
+  width: 50%;
+`;
+
+export const BEANIE_DIV = styled.div `
+  background: white;
+  border: solid lightgray;
+  display: flex;
+  float: left;
+  height: 350px;
+  padding: 65px;
+  position: relative;
+  margin-left: 10%;
+  margin-top: 10px;
+  width: 350px;
+`;
+
+export const BANNER_DIV = styled.div `
+  background-color: #89d4cf;
+  background-image: linear-gradient(315deg, #89d4cf 0%, #6e45e1 74%);
+  height: 150px;
+`;
